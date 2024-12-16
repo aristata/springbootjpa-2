c0f2f38151098852adcfe48f6fd9112877d7799b
장성민 <aristatait@gmail.com>
2024-12-16 11:00:58 +0900

    🐛 fix: 빌드 버그 픽스와 로그 줄바꿈 취소

--- src/test/java/kr/co/aristatait/springbootjpa1/service/MemberServiceTest.java
+++ src/test/java/kr/co/aristatait/springbootjpa1/service/MemberServiceTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,7 +24,6 @@ class MemberServiceTest {
     MemberRepository memberRepository;
 
     @Test
-    @Rollback(false)
     public void 회원가입() throws Exception {
         //given
         Member member = new Member();
